9e50d6b403e802925d3a5373318a432dec117292
Alexei <61819719+Alexes012@users.noreply.github.com>
2020-07-01 18:44:41 +0300

    Updates

---
+++ precache-manifest.d07678d664234acb05c1e50063706444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d1ddea11703abcb3f26759730f16c6",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e83f32ff7a447fc4ddce",
+    "url": "/calculator/static/css/main.320b8d08.chunk.css"
+  },
+  {
+    "revision": "8311b051dc57b30fc36a",
+    "url": "/calculator/static/js/2.a942f5e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.a942f5e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e83f32ff7a447fc4ddce",
+    "url": "/calculator/static/js/main.bcfa1e0f.chunk.js"
+  },
+  {
+    "revision": "f80ccc7a9cc485904dcb",
+    "url": "/calculator/static/js/runtime-main.d8230d6e.js"
+  }
+]);
